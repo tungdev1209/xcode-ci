c4f7009b8d1bab5e467efab142e4ec823bebd5c6
TungNQ4 <tungnguyen.dev@gmail.com>
2019-01-09 22:48:11 +0700

    make exe file

--- ios-ci.rb
+++ ios-ci.rb
@@ -1,11 +1,11 @@
 class IosCi < Formula
   desc "Build, Archive & Export iOS/MacOS app via command-line"
   homepage "https://github.com/tungdev1209/homebrew-ios-ci"
-  url "https://github.com/tungdev1209/homebrew-ios-ci/archive/v1.0.4.tar.gz"
+  url "https://github.com/tungdev1209/homebrew-ios-ci/archive/v1.0.5.tar.gz"
   sha256 "3a6a9b9bc02af8da4c15695b0cc686cbc28239735f41e01a0845008340966a3b"
 
   def install
-    bin.install "ios-ci.sh"
+    bin.install "ios-ci"
     prefix.install "Author"
     prefix.install "README.md"
     prefix.install "hooks"
