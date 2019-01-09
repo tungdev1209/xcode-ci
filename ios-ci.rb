3f54589163fe31cc4c078efb85f54fc6b2b84046
TungNQ4 <tungnq4@fsoft.com.vn>
2019-01-09 12:24:37 +0700

    fix for release

--- ios-ci.rb
+++ ios-ci.rb
@@ -1,16 +1,22 @@
 class IosCi < Formula
   desc "Build, archive & export iOS/MacOS app via command-line"
   homepage "https://github.com/tungdev1209/homebrew-ios-ci"
-  version "1.0.3"
   url "https://github.com/tungdev1209/homebrew-ios-ci/archive/v1.0.3.tar.gz"
   sha256 "adc7192ebd09dbb994b3245739ec7eb0a4c8b76030dbbae85fe94ddff36637b7"
 
   def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--disable-maintainer-mode",
+                          "--prefix=#{prefix}"
+
     bin.install "ios-ci"
     prefix.install "Author"
     prefix.install "README.md"
     prefix.install "hooks"
     prefix.install "config"
+    
+    system "make", "install"
   end
 
   test do
