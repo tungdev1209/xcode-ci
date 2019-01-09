7875b6c86a20085bf856d61b1b0de7e17fe0c787
TungNQ4 <tungnq4@fsoft.com.vn>
2019-01-09 11:10:15 +0700

    update sha

--- ios-ci.rb
+++ ios-ci.rb
@@ -3,7 +3,7 @@ class IosCi < Formula
   homepage "https://github.com/tungdev1209/homebrew-ios-ci"
   version "1.0.3"
   url "https://github.com/tungdev1209/homebrew-ios-ci/archive/v1.0.3.tar.gz"
-  sha256 "f1bc1c38bd08238867b8fb5d08fd7d893bd80e550243a793e6c088ed81f65bb4"
+  sha256 "adc7192ebd09dbb994b3245739ec7eb0a4c8b76030dbbae85fe94ddff36637b7"
 
   def install
     bin.install "ios-ci"
