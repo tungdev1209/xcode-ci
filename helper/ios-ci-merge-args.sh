5f0b18377c2c92d5f0100bb8a3393bf10f87afe4
TungNQ4 <tungnguyen.dev@gmail.com>
2019-01-15 01:03:29 +0700

    fix for add more specific params when make build cmd

--- helper/ios-ci-merge-args.sh
+++ helper/ios-ci-merge-args.sh
@@ -4,34 +4,112 @@ helper_path=$(dirname "$0")
 
 # check input argument
 start_value="0"
+start_keyvalue_single_comma="0"
+start_keyvalue_double_comma="0"
+start_value_double_comma="0"
 cmd_value=""
 ci_cmd_args=";$1"
 declare -i arg_num=0
 for var in "$@"
 do
     arg_num=$(( arg_num + 1 ))
-    if (( arg_num > 1 )); then # already keep $1
-        if [ "$start_value" == "1" ]; then
-            if [ "${var: -1}" == "\\" ]; then # convert var to beauty string
-                var="${var:0:$((${#var}-1))}'"
-            fi
-            value=$var
-            if [ "${var: -1}" == "'" ]; then
-                start_value="0"
-            else
-                value+=" "
-            fi
-            cmd_value+=$value
-            if [ "$start_value" == "0" ]; then
-                ci_cmd_args+=";${cmd_value}"
-                cmd_value=""
-            fi
-        elif [ "${var:0:1}" == "\\" ] || [ "${var:0:1}" == "'" ] ; then
+    if (( arg_num == 1 )); then # already keep $1
+        continue
+    fi
+    if [ "$start_value" == "1" ]; then
+        if [ "${var: -1}" == "\\" ]; then # convert var to beauty string
+            var="${var:0:$((${#var}-1))}'"
+        fi
+
+        value=$var
+
+        if [ "${var: -1}" == "'" ]; then
+            start_value="0"
+        else
+            value+=" "
+        fi
+
+        cmd_value+=$value
+        
+        if [ "$start_value" == "0" ]; then
+            ci_cmd_args+=";${cmd_value}"
+            cmd_value=""
+        fi
+        continue
+    fi
+    if [ "$start_keyvalue_double_comma" == "1" ]; then
+        value=$var
+
+        if [ "${var: -1}" == "\"" ]; then
+            start_keyvalue_double_comma="0"
+        else
+            value+=" "
+        fi
+
+        cmd_value+=$value
+        
+        if [ "$start_keyvalue_double_comma" == "0" ]; then
+            ci_cmd_args+=";${cmd_value}"
+            cmd_value=""
+        fi
+        continue
+    fi
+
+    if [ "$start_value_double_comma" == "1" ]; then
+        value=$var
+
+        if [ "${var: -1}" == "\"" ]; then
+            start_value_double_comma="0"
+        else
+            value+=" "
+        fi
+
+        cmd_value+=$value
+        
+        if [ "$start_value_double_comma" == "0" ]; then
+            ci_cmd_args+=";${cmd_value}"
+            cmd_value=""
+        fi
+        continue
+    fi
+
+    if [ "${var:0:1}" == "'" ]; then # -destination 'platform=iOS Simulator,name=iPad Air 2,OS=11.3'
+        if [ "${var: -1}" != "'" ]; then
             start_value="1"
-            cmd_value="'${var:1}" # convert var to beauty string
+            cmd_value="${var} "
         else
             ci_cmd_args+=";${var}"
         fi
+    elif [ "${var:0:1}" == "\\" ]; then # -destination \"platform=iOS Simulator,name=iPad Air 2,OS=11.3\"
+        if [ "${var: -1}" != "\\" ]; then
+            start_value="1"
+            cmd_value="'${var:1} " # convert var to beauty string
+        else
+            ci_cmd_args+=";\"${var:1:$((${#var}-2))}\""
+        fi
+    elif [ "${var:0:1}" == "\"" ]; then # -destination "platform=iOS Simulator,name=iPad Air 2,OS=11.3"
+        if [ "${var: -1}" != "\"" ]; then
+            start_value_double_comma="1"
+            cmd_value="\"${var:1} " # convert var to beauty string
+        else
+            ci_cmd_args+=";${var}"
+        fi
+    elif [[ ${var} == *"='"* ]]; then
+        if [ "${var: -1}" != "'" ]; then # PROVISIONING_PROFILE_SPECIFIER='HelloWorld Provisioning Development'
+            start_value="1"
+            cmd_value="$var "
+        else
+            ci_cmd_args+=";${var}"
+        fi
+    elif [[ ${var} == *"=\""* ]]; then
+        if [ "${var: -1}" != "\"" ]; then # PROVISIONING_PROFILE_SPECIFIER="HelloWorld Provisioning Development"
+            start_keyvalue_double_comma="1"
+            cmd_value="$var "
+        else
+            ci_cmd_args+=";${var}"
+        fi
+    else
+        ci_cmd_args+=";${var}"
     fi
 done
 args=$(python ${helper_path}/py_merge_args.py -a "${ci_cmd_args}")
--- helper/py_merge_args.py
+++ helper/py_merge_args.py
@@ -30,15 +30,32 @@ for arg in args_array:
     if arg[0:1] == '-':
         is_new_key = True
         current_key = arg
+    elif '=' in arg and arg[0:1] != "\"" and arg[0:1] != "'":
+        kv = arg.split('=')
+        a_key = kv[0] + '='
+        index = 0
+        a_value = ''
+        for v in kv:
+            index += 1
+            if index == 1:
+                continue
+            if index == 2:
+                a_value += v
+                continue
+            a_value += '=' + v
+        keys_values[a_key] = a_value
     elif arg != "":
         keys_values[arg] = ''
 
 full_args = ''
 for key in keys_values:
-    full_args += key + ' '
+    full_args += key
     value = keys_values[key]
-    if value != "":
-        full_args += value + ' '
+    if key[-1:] == '=':
+        full_args += value
+    elif value != "":
+        full_args += ' ' + value
+    full_args += ' '
 print full_args
 
         
