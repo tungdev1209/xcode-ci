a67869ea3cbacceb502254d8762bfb297960e74f
TungNQ <tungnq4@fsoft.com.vn>
2019-01-15 17:44:55 +0700

    remove hardcode for release

--- ios-ci.rb
+++ ios-ci.rb
@@ -1,7 +1,7 @@
 class IosCi < Formula
   desc "Build, Archive & Export iOS/MacOS app via command-line"
   homepage "https://tungdev1209.github.io/homebrew-ios-ci/"
-  url "https://github.com/tungdev1209/homebrew-ios-ci/archive/v1.0.20.tar.gz"
+  url "https://github.com/tungdev1209/homebrew-ios-ci/archive/v1.0.21.tar.gz"
   sha256 "cdeabc901c19d721df08c915554e37e30ef695d06df99724c70c3b758405cb84"
 
   def install
