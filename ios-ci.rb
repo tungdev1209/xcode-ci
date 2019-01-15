a8857955d4211412727fc93f85ed3e8e6109e54f
TungNQ <tungnq4@fsoft.com.vn>
2019-01-15 17:45:26 +0700

    release 1.0.21

--- ios-ci.rb
+++ ios-ci.rb
@@ -2,7 +2,7 @@ class IosCi < Formula
   desc "Build, Archive & Export iOS/MacOS app via command-line"
   homepage "https://tungdev1209.github.io/homebrew-ios-ci/"
   url "https://github.com/tungdev1209/homebrew-ios-ci/archive/v1.0.21.tar.gz"
-  sha256 "cdeabc901c19d721df08c915554e37e30ef695d06df99724c70c3b758405cb84"
+  sha256 "60ffbb678ba3cbe6237f10f414c4080d9cbd19a6205d14fa458f252b95837d96"
 
   def install
     bin.install "ios-ci"
